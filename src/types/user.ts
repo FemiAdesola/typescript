ce466487ec2db2ea1334b6f98e8461e8e80f074d
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2022-12-15 14:00:08 +0200

    added interface into user.ts and product.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,4 +2,10 @@ import { Entity } from "./common";
 
 export interface User extends Entity{
     /* complete User interface with some chosen properties */
+    name: string;
+    email: string;
+    password: string;
+    role: "customer"| "admin";
+    avatar: string;
+    id: number;
 }
